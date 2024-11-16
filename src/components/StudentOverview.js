ff58a798a89017574e4252f7a97b826732a98614
rakibul44 <mdrakib76634@gmail.com>
2024-11-16 12:15:57 +0600

    DashBoard added

--- src/components/StudentOverview.js
+++ src/components/StudentOverview.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './studentOverview.css';
+import p2 from '../images/p2.jpg';
 
 const StudentOverview = () => {
   return (
@@ -10,15 +11,18 @@ const StudentOverview = () => {
             <div class="col-md-3">
                 <div class="sidebar">
                     <div class="profile-info mb-4">
-                        <img src="https://via.placeholder.com/80" alt="Profile" class="rounded-circle mb-2"></img>
+                        <img src={p2} alt="Profile" class="rounded-circle profile mb-2"></img>
                         <h5>User Points</h5>
-                        <p>0.00</p>
+                        <p>10.00</p>
                     </div>
-                    <div>
-                        <p><i class="fas fa-th-large"></i> Dashboard</p>
-                        <p><i class="fas fa-book"></i> My Courses</p>
-                        <p><i class="fas fa-check-square"></i> Completed Courses</p>
-                        <p><i class="fas fa-user"></i> Profile</p>
+                    <div class = 'sidebar-menu'>
+                        <Link to='#'><i class="fas fa-th-large"></i> Dashboard</Link>
+                        <Link to='#'><i class="fas fa-book"></i> My Courses</Link>
+                        <Link to='#'><i class="fas fa-check-square"></i> Completed Courses</Link>
+                        <Link to='#'><i class="fas fa-user"></i> My Certificates</Link>
+                        <Link to='#'><i class="fas fa-user"></i> Refar and Earn</Link>
+                        <Link to='#'><i class="fas fa-user"></i> Profile</Link>
+                        <Link to='#'><i class="fas fa-user"></i> Sign-Out</Link>
                     </div>
                 </div>
             </div>
@@ -29,26 +33,33 @@ const StudentOverview = () => {
                         <div class="p-3 border rounded">
                             <h5>Enrolled Courses</h5>
                             <h3>3</h3>
-                            <button class="btn btn-view">View All</button>
+                            <Link to='#'>
+                            <button class="btn btn-view">View All</button>                            
+                            </Link>
                         </div>
                     </div>
                     <div class="col-md-4">
                         <div class="p-3 border rounded">
                             <h5>Completed Courses</h5>
                             <h3>1</h3>
-                            <button class="btn btn-view">View All</button>
+                            <Link to='#'>
+                            <button class="btn btn-view">View All</button>                            
+                            </Link>
                         </div>
                     </div>
                     <div class="col-md-4">
                         <div class="p-3 border rounded">
                             <h5>Ongoing Courses</h5>
                             <h3>2</h3>
-                            <button class="btn btn-view">View All</button>
+                            <Link to='#'>
+                            <button class="btn btn-view">View All</button>                            
+                            </Link>
                         </div>
                     </div>
                 </div>
 
                 <h4>You have completed 1 course!</h4>
+                {/* Cards area start */}
                 <div class="row">
                     <div class="col-md-4 mb-4">
                     <div class="card w-100">
@@ -59,14 +70,14 @@ const StudentOverview = () => {
                             <div class="progress mb-2">
                                 <div class="progress-bar" role="progressbar"  aria-valuenow="18" aria-valuemin="0" aria-valuemax="100">18% completed</div>
                                 </div>
-                                <button class="btn btn-outline-dark btn-block">Continue</button>
+                                <Link to='/preview'>
+                                    <button class="btn btn-view">Continue</button>                            
+                                </Link>
                                 </div>
                                 </div>
                             </div>
-
-                    
-
-                            <div class="col-md-4 mb-4">
+                            {/* Cards */}
+                    <div class="col-md-4 mb-4">
                     <div class="card w-100">
                         <img src="https://cdn.ghoorilearning.com/uploads//learning/images/20221213145904-63983ed8940b0.jpeg" class="card-img-top" alt="..."></img>
                         <div class="card-body">
@@ -75,12 +86,14 @@ const StudentOverview = () => {
                             <div class="progress mb-2">
                                 <div class="progress-bar" role="progressbar"  aria-valuenow="18" aria-valuemin="0" aria-valuemax="100">18% completed</div>
                                 </div>
-                                <button class="btn btn-outline-dark btn-block">Continue</button>
+                                <Link to='/preview'>
+                                    <button class="btn btn-view">Continue</button>                            
+                                </Link>
                                 </div>
                                 </div>
                             </div>
                             <div class="col-md-4 mb-4">
-                    <div class="card w-100">
+                        <div class="card w-100">
                         <img src="https://cdn.ghoorilearning.com/uploads//learning/images/20221213145904-63983ed8940b0.jpeg" class="card-img-top" alt="..."></img>
                         <div class="card-body">
                             <h5 class="card-title">Quran Shikkha</h5>
@@ -88,15 +101,16 @@ const StudentOverview = () => {
                             <div class="progress mb-2">
                                 <div class="progress-bar" role="progressbar"  aria-valuenow="18" aria-valuemin="0" aria-valuemax="100">18% completed</div>
                                 </div>
-                                <button class="btn btn-outline-dark btn-block">Continue</button>
-                                </div>
-                                </div>
+                                <Link to='/preview'>
+                                    <button class="btn btn-view">Continue</button>                            
+                                </Link>
                             </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-
     </div>
   );
 };
